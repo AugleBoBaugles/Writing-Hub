a74f3238559b3e85d5b6b4a3f551d0f47cb2fd16
Augy Markham <katiefaithmarkham@gmail.com>
2024-12-04 19:39:33 -0800

    Added Styles and new pages

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express();
 const PORT = 3000;
 
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static('public'));
+
 
 app.set('view engine', 'ejs');
 
@@ -10,6 +12,15 @@ app.get('/', (req, res) => {
     console.log("Hello, world - server!");
 
     res.render('home');
+});
+
+app.get('/request', (req, res) => {
+    res.render('request');
+});
+
+app.post('/submit', (req, res) => {
+    const data = req.body;
+    res.render('submitted');
 });
 
 app.listen(PORT, () => {
